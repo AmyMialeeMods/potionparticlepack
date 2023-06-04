c02e298779d5e784aab00e090d9f05a7893e9d48
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-06-04 08:40:08 +0100

    Update ItemRendererMixin.java

--- src/main/java/xyz/amymialee/potionparticlepack/mixin/ItemRendererMixin.java
+++ src/main/java/xyz/amymialee/potionparticlepack/mixin/ItemRendererMixin.java
@@ -3,6 +3,7 @@ package xyz.amymialee.potionparticlepack.mixin;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.BufferRenderer;
 import net.minecraft.client.render.GameRenderer;
@@ -20,6 +21,7 @@ import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -33,26 +35,40 @@ public abstract class ItemRendererMixin implements SynchronousResourceReloader {
     @Inject(method = "renderGuiItemOverlay(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V", at = @At("TAIL"))
     private void potionParticlePack$letsSeeTheEffects(MatrixStack matrices, TextRenderer textRenderer, ItemStack stack, int x, int y, String countLabel, CallbackInfo ci) {
         List<StatusEffectInstance> collection = PotionUtil.getPotionEffects(stack);
-        if (this.client.player != null) {
-            for (StatusEffectInstance instance : collection) {
+        if (this.client.player != null && Screen.hasShiftDown()) {
+            for (int i = 0; i < collection.size(); i++) {
+                StatusEffectInstance instance = collection.get(i);
                 RenderSystem.enableBlend();
-                Sprite sprite = this.client.getStatusEffectSpriteManager().getSprite(collection.get((this.client.player.age / 20) % collection.size()).getEffectType());
+                Sprite sprite = this.client.getStatusEffectSpriteManager().getSprite(instance.getEffectType());
                 RenderSystem.setShaderTexture(0, sprite.getAtlasId());
-                int size = 9;
+                int size = 18 / collection.size();
                 RenderSystem.setShader(GameRenderer::getPositionTexProgram);
                 RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-                Matrix4f matrix = matrices.peek().getPositionMatrix();
-                float xOffset = -1; // + -MathHelper.sin((this.client.player.age + this.client.getTickDelta()) * (float) (Math.PI / 180.0) * 4) * 6;
-                float yOffset = -1; // + MathHelper.cos((this.client.player.age + this.client.getTickDelta()) * (float) (Math.PI / 180.0) * 4) * 6;
-                BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
-                bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
-                bufferBuilder.vertex(matrix, x + xOffset, y + yOffset, 200).texture(sprite.getMinU(), sprite.getMinV()).next();
-                bufferBuilder.vertex(matrix, x + xOffset, y + yOffset + size, 200).texture(sprite.getMinU(), sprite.getMaxV()).next();
-                bufferBuilder.vertex(matrix, x + xOffset + size, y + yOffset + size, 200).texture(sprite.getMaxU(), sprite.getMaxV()).next();
-                bufferBuilder.vertex(matrix, x + xOffset + size, y + yOffset, 200).texture(sprite.getMaxU(), sprite.getMinV()).next();
-                BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+                int xOffset = -1 + (18 / collection.size() * (i));
+                int yOffset = -1 + (18 - size) / 2;
+                RenderSystem.setShaderColor(0.0f, 0.0f, 0.0f, 0.1f); // * MathHelper.sin((float) (this.client.player.age * (Math.PI / 180.0) * 4)));
+                for (int j = -1; j <= 1; j++) {
+                    for (int k = -1; k <= 1; k++) {
+                        if (j == 0 && k == 0) continue;
+                        this.renderIcon(matrices, sprite, x, y, size, xOffset + (j), yOffset + (k));
+                    }
+                }
+                RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f); //, MathHelper.sin((float) (this.client.player.age * (Math.PI / 180.0) * 4)));
+                this.renderIcon(matrices, sprite, x, y, size, xOffset, yOffset);
                 RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
             }
         }
+    }
+
+    @Unique
+    private void renderIcon(MatrixStack matrices, Sprite sprite, int x, int y, int size, float xOffset, float yOffset) {
+        Matrix4f matrix = matrices.peek().getPositionMatrix();
+        BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+        bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+        bufferBuilder.vertex(matrix, x + xOffset, y + yOffset, 200).texture(sprite.getMinU(), sprite.getMinV()).next();
+        bufferBuilder.vertex(matrix, x + xOffset, y + yOffset + size, 200).texture(sprite.getMinU(), sprite.getMaxV()).next();
+        bufferBuilder.vertex(matrix, x + xOffset + size, y + yOffset + size, 200).texture(sprite.getMaxU(), sprite.getMaxV()).next();
+        bufferBuilder.vertex(matrix, x + xOffset + size, y + yOffset, 200).texture(sprite.getMaxU(), sprite.getMinV()).next();
+        BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
     }
 }
